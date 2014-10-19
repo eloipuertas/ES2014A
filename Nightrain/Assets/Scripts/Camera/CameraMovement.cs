6a13d1cc530157c1d95b27f879d0abb289f00164
orquidea1992 <tian.superboy.lan50@gmail.com>
2014-10-20 00:04:58 +0200

    zoom limit corrected

--- Nightrain/Assets/Scripts/Camera/CameraMovement.cs
+++ Nightrain/Assets/Scripts/Camera/CameraMovement.cs
@@ -6,8 +6,8 @@ public class CameraMovement : MonoBehaviour {
 	public Transform target;					//Target position
 	private Transform _myTransform; 			//Camera position 
 	
-	private float distance = 20.0f;
-	private float height = 20.0f;
+	private float distance;
+	private float height;
 	private Vector3 scaleTarget;
 	
 	
@@ -26,22 +26,31 @@ public class CameraMovement : MonoBehaviour {
 	private void CameraSetUp(){
 		scaleTarget = target.localScale;
 		_myTransform = transform;
+
+		//Set height and distance
+		height = scaleTarget[1] * 30.0f;
+		distance = scaleTarget [2] * 20.0f;
 	}
 	
 	void LateUpdate(){
 		//If a target is setted to the camera in order to follow it 
 		if(target != null){
 			//We can use the mouse wheel to zoom
-			height += Input.GetAxis("Mouse ScrollWheel") * -10;
-			distance += Input.GetAxis("Mouse ScrollWheel") * -10;
+			height += Input.GetAxis("Mouse ScrollWheel") * -27;
+			distance += Input.GetAxis("Mouse ScrollWheel") * -20;
 			
-			if(height < 1.0f) height = 1.0f;
-			if(distance < 2.0f) distance = 2.0f;
-			
+			//Limits zoom
+			if(height < scaleTarget[1] * 15.0f) height = scaleTarget[1] * 15.0f;
+			if(height > scaleTarget[1] * 60.0f) height = scaleTarget[1] * 60.0f;
+			if(distance < scaleTarget [2] * 10.0f) distance = scaleTarget [2] * 10.0f;
+			if(distance > scaleTarget [2] * 40.0f) distance = scaleTarget [2] * 40.0f;
+
+			Debug.LogWarning(scaleTarget);
+
 			//In each frame we get the position of the target and we update the position camera
 			_myTransform.position = new Vector3(target.position.x + distance,
-			                                    target.position.y + height * scaleTarget[1],
-			                                    target.position.z - distance * scaleTarget[2]);
+			                                    target.position.y + height,
+			                                    target.position.z - distance);
 			//We must update LookAt because the target can change position
 			_myTransform.LookAt(target);
 		}
