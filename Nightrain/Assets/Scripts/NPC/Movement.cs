749a4815f6e68e79b0c53925e72f07ce46ac743c
Daniel Urdas <urdas.daniel@yahoo.com>
2014-10-19 12:11:10 +0200

    NPC atack
    preparar el script del NPC para poder atacar y recibir daños

--- Nightrain/Assets/Scripts/NPC/Movement.cs
+++ Nightrain/Assets/Scripts/NPC/Movement.cs
@@ -2,26 +2,41 @@
 using System.Collections;
 
 public class Movement : MonoBehaviour {
+	private float health = 100;
+	private string state;
+	private float damageAtack = 5;
+	
 	private float rotationSpeed = 1.0f;
 	private int moveSpeed = 20;
-	int[] degreesList = {90,180,270,360};
+	
 	// Los nombres de los tres puntos que estan distribuidos por el mapa
 	string[] points = {"Point1", "Point2", "Point3"};
-	int i = 0, j=0;
-	int delay = 300;
+	int j = 0;
+
+	GameObject player;
+	Transform player_transform;
 	
 	// Use this for initialization
 	void Start () {
-		// Mas adelante inicializar atributos del NPC
+		player = GameObject.FindGameObjectWithTag("Player");
+		player_transform = player.transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		seguirPuntos();
+		// Cosas a determinar por el programador de IA
+		float distance_to_player = Vector3.Distance(player_transform.position,transform.position);
+		Debug.Log (distance_to_player);
+		if(distance_to_player < 15){
+			atack();
+		}else{
+			seguirPuntos();
+		}
 	}
 
 	// Metodo que hace que el personaje vaya uno a uno a los tres puntos del mapa
 	void seguirPuntos(){
+		//Debug.Log ("caminar");
 		GameObject punto = GameObject.Find(points[j]);
 		// Calculamos la distancia entre nuestra posicion y el punto del mapa
 		float distancia = Vector3.Distance(transform.position, punto.transform.position);
@@ -39,26 +54,24 @@ public class Movement : MonoBehaviour {
 		transform.position += transform.forward * moveSpeed * Time.deltaTime;
 	}
 
-	// Camina, rotandose 90 grados cada cierto tiempo
-	void caminar(){
-		// Transladamos el NPC hacia adelante
-		transform.position += transform.forward * moveSpeed * Time.deltaTime;
-		delay--;
-		// Cada cierto tiempo movemos el personaje 90 grados a la derecha
-		if (delay < 0) {
-			delay = 300;
-			rotar (degreesList[i]);
-			i++;
-			if (i > 3) {
-				i = 0;
-			}
-		}
-	}
 
 	// Metodo que rota el NPC unos determinados grados
 	void rotar(int degrees){
 		Quaternion newRotation = Quaternion.AngleAxis (degrees, Vector3.up);
 		transform.rotation = Quaternion.Slerp (transform.rotation, newRotation, rotationSpeed);
 	}
-	
+
+
+	void atack(){
+		//Debug.Log ("atack");
+	}
+
+
+	public void getDamage(float damage){
+		health -= damage;
+		if (health < 1) {
+			Debug.Log ("NPC muerto");
+			Destroy (gameObject);
+		}
+	}
 }
