46db9d0b49e87e8f230986e7a12980710bb9ff1b
Iván Álvarez Ferreiro <ivarg.177@gmail.com>
2014-10-20 20:49:51 +0200

    update

--- Nightrain/Assets/Scripts/Level01/GameEngineLevel01.cs
+++ Nightrain/Assets/Scripts/Level01/GameEngineLevel01.cs
@@ -9,6 +9,7 @@ public class GameEngineLevel01 : MonoBehaviour {
 
 	public Transform[] prefab;
 	private GameObject character;
+	private GameObject npc;
 	// Use this for initialization
 	void Start () {
 		Cursor.SetCursor(cursor[0], hotSpot, mode);
@@ -24,6 +25,10 @@ public class GameEngineLevel01 : MonoBehaviour {
 						Instantiate (prefab [2]);
 
 		this.character = GameObject.FindGameObjectWithTag ("Player");
+
+		GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>() ;
+		foreach(object go in allObjects)
+				print(go) ;
 		//this.character.transform.position = Vector3.zero;
 		//this.character.transform.rotation = Quaternion.identity;
 
@@ -32,6 +37,6 @@ public class GameEngineLevel01 : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
