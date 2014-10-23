25a3fdcbe53fd4ca3f902f29431d9f76efaa9033
francescbosch <Francesc@Francesc-PC>
2014-10-23 02:59:07 +0200

    añadir animaciones y solucionar problemas barriles

---
+++ Nightrain/Assets/Models/Barriles/Scripts/barrel-anim.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var max=10;
+var expand=0.25;
+private var x =0;
+private var y=max;
+
+function Update () {
+	if(x<max){
+		transform.localScale += Vector3(expand,expand,expand);
+		x++;
+	}
+	else if(x==max){
+		y=0;
+		x=max+1;
+	}
+	if(y<max){
+		transform.localScale += Vector3(-expand,-expand,-expand);
+		y++;
+	}
+	else if(y==max){
+		x=0;
+		y=max+1;
+	}
+}
