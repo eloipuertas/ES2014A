18c0daadad5a8be3b44b8b089c316664017fca5c
Daniel Urdas <urdas.daniel@yahoo.com>
2014-10-24 12:52:43 +0200

    corregir errores atacar

--- Nightrain/Assets/Scripts/NPC/Movement.cs
+++ Nightrain/Assets/Scripts/NPC/Movement.cs
@@ -7,12 +7,15 @@ public class Movement : MonoBehaviour {
 	private float damageAtack = 5;
 	
 	private float rotationSpeed = 1.0f;
-	private int moveSpeed = 20;
+	private int moveSpeed = 5;
+
+	private Animator anim;
+	private float attackTime;
 	
 	// Los nombres de los tres puntos que estan distribuidos por el mapa
 	string[] points = {"Point1", "Point2", "Point3"};
 	int j = 0;
-
+	
 	GameObject player;
 	Transform player_transform;
 	
@@ -20,32 +23,38 @@ public class Movement : MonoBehaviour {
 	void Start () {
 		player = GameObject.FindGameObjectWithTag("Player");
 		player_transform = player.transform;
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// Cosas a determinar por el programador de IA
 		float distance_to_player = Vector3.Distance(player_transform.position,transform.position);
+		//atack ();
 		Debug.Log (distance_to_player);
-		if(distance_to_player < 15){
-			atack();
-		}else{
+		if (distance_to_player < 8) {
+			atack ();
+		} else if (distance_to_player < 60) {
+			perseguir();
+		} else {
 			seguirPuntos();
 		}
 	}
-
+	
 	// Metodo que hace que el personaje vaya uno a uno a los tres puntos del mapa
 	void seguirPuntos(){
-		//Debug.Log ("caminar");
+		anim.SetBool("w_attack", false);
+		anim.SetBool("a_walk", true);
+		anim.SetBool ("walk", true);
 		GameObject punto = GameObject.Find(points[j]);
 		// Calculamos la distancia entre nuestra posicion y el punto del mapa
 		float distancia = Vector3.Distance(transform.position, punto.transform.position);
 		// Si ya hemos llegado al punto, cambiamos la i para ir al siguiente
-		if (distancia <= 1) {
+		if (distancia <= 15) {
 			if (j < 2) {
-					j++;
+				j++;
 			} else {
-					j = 0;
+				j = 0;
 			}
 		}
 		// Rotamos hacia la direccion
@@ -53,25 +62,57 @@ public class Movement : MonoBehaviour {
 		// Transladamos el NPC hacia el punto
 		transform.position += transform.forward * moveSpeed * Time.deltaTime;
 	}
-
-
+	
+	void perseguir(){
+		anim.SetBool("w_attack", false);
+		anim.SetBool("a_walk", true);
+		anim.SetBool ("walk", true);
+		Vector3 p= player_transform.position;
+		p.y = transform.position.y;
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(p - transform.position), rotationSpeed * Time.deltaTime);
+		transform.position += transform.forward * moveSpeed * 2 * Time.deltaTime;
+	}
+	
+	
 	// Metodo que rota el NPC unos determinados grados
 	void rotar(int degrees){
 		Quaternion newRotation = Quaternion.AngleAxis (degrees, Vector3.up);
 		transform.rotation = Quaternion.Slerp (transform.rotation, newRotation, rotationSpeed);
 	}
-
-
+	
+	
 	void atack(){
-		//Debug.Log ("atack");
+		Vector3 p= player_transform.position;
+		p.y = transform.position.y;
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(p - transform.position), rotationSpeed * Time.deltaTime);
+		Debug.Log("attack");
+		anim.SetBool("a_walk", false);
+		anim.SetBool("walk", false);
+		anim.SetBool ("w_attack", true);
+		if (Time.time > attackTime) {
+			// atacar(verificar colision con el player y enviarle via metodo que ha sido atacado)
+			//player.GetComponent<CharacterScript>().setDamage(10);
+			attackTime = Time.time + 1.0f;
+		}
 	}
-
-
-	public void getDamage(float damage){
+	
+	
+	public void setDamage(float damage){
 		health -= damage;
 		if (health < 1) {
 			Debug.Log ("NPC muerto");
 			Destroy (gameObject);
 		}
 	}
+
+
+	/*void OnTriggerEnter (Collider other){
+		print("Tocado."); 
+		if(other.gameObject == this.player){
+			this.player.GetComponent<CharacterScript>().setDamage(10);
+			print("Tocado."); 
+			Debug.Log("Debug:Tocado.");
+			System.Console.WriteLine("System:Tocado");	
+		}	
+	}*/
 }
