fc3f028d7a4d37b85fbba204a34278da11876516
Ivan Alcoba <ivanalcoba92@gmail.com>
2014-11-10 13:03:07 +0100

    Added sound to golems attacks
    Golem plays a sound when attacks the player

---
+++ Nightrain/Assets/Scripts/Music/Music_Engine_Script.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music_Engine_Script : MonoBehaviour {
+
+	public AudioClip npc_golem_attack;
+	public AudioClip npc_golem_agresive;
+	public AudioClip character_attack;
+	public AudioClip character_hurt;
+	public AudioClip character_steps;
+	public AudioClip barrel_open;
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	public void play_Golem_Attack() {
+		Debug.Log ("Reproduciendo sonido");
+		audio.PlayOneShot (npc_golem_attack);
+	}
+	
+	public void play_Golem_Agresive() {
+		Debug.Log ("Reproduciendo sonido");
+		audio.PlayOneShot (npc_golem_agresive);
+	}
+	
+	public void play_Player_Sword_Attack() {
+		Debug.Log ("Reproduciendo sonido");
+		audio.PlayOneShot (character_attack);
+	}
+	
+	public void play_Player_Hurt() {
+		Debug.Log ("Reproduciendo sonido");
+		audio.PlayOneShot (character_hurt);
+	}
+	
+	public void play_Player_Steps() {
+		Debug.Log ("Reproduciendo sonido");
+		audio.PlayOneShot (character_steps);
+	}
+	
+	public void stop_Player_Steps() {
+		Debug.Log ("Reproduciendo sonido");
+		audio.Stop ();
+	}
+	
+	public void Play_Barrel_Open() {
+		Debug.Log ("Reproduciendo sonido");
+		audio.PlayOneShot (barrel_open);
+	}
+}
