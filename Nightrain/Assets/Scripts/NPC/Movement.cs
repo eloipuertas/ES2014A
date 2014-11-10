9b01eedc4601e0f39da14486decd23772c6616f8
Daniel Urdas <urdas.daniel@yahoo.com>
2014-11-10 12:49:51 +0100

    añadir atributos personaje

--- Nightrain/Assets/Scripts/NPC/Movement.cs
+++ Nightrain/Assets/Scripts/NPC/Movement.cs
@@ -2,24 +2,27 @@
 using System.Collections;
 
 public class Movement : MonoBehaviour {
+	//##############################
+	//Atributos personaje
+	private float moveSpeed = 5; 
 	private float health = 100;
-	private string state;
-	private float damageAtack = 5;
-	
-	private float rotationSpeed = 1.0f;
-	private int moveSpeed = 5;
-
-	private Animator anim;
-	private float attackTime;
-
 	private float max_health = 100;
-	
+	private float defense = 10;
+	private float attackPower = 5;
+	//##############################
+
+	private string state;
+	string[] states = {"Walk", "Find", "Attack", "Dead"};
+	private float rotationSpeed = 1.0f;
+	private float attackTime = 3.0f;
+
 	// Los nombres de los tres puntos que estan distribuidos por el mapa
 	string[] points = {"Point1", "Point2", "Point3"};
 	int j = 0;
 	
-	GameObject player;
-	Transform player_transform;
+	private GameObject player;
+	private Transform player_transform;
+	private Animator anim;
 
 	private GameObject NPCbar;
 	
@@ -27,8 +30,7 @@ public class Movement : MonoBehaviour {
 	void Start () {
 		player = GameObject.FindGameObjectWithTag("Player");
 		player_transform = player.transform;
-		anim = GetComponent<Animator>();
-
+		anim = GetComponent<Animator> ();
 		this.NPCbar = GameObject.FindGameObjectWithTag("NPCHealth");
 	}
 	
@@ -46,13 +48,9 @@ public class Movement : MonoBehaviour {
 		}
 	}
 
-	//Return health
-	public float getHealth(){
-		return health;
-	}
-	
 	// Metodo que hace que el personaje vaya uno a uno a los tres puntos del mapa
 	void seguirPuntos(){
+		state = "Walk";
 		anim.SetBool("w_attack", false);
 		anim.SetBool("a_walk", true);
 		anim.SetBool ("walk", true);
@@ -74,6 +72,7 @@ public class Movement : MonoBehaviour {
 	}
 	
 	void perseguir(){
+		state = "Find";
 		anim.SetBool("w_attack", false);
 		anim.SetBool("a_walk", true);
 		anim.SetBool ("walk", true);
@@ -92,6 +91,7 @@ public class Movement : MonoBehaviour {
 	
 	
 	void atack(){
+		state = "Attack";
 		Vector3 p= player_transform.position;
 		p.y = transform.position.y;
 		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(p - transform.position), rotationSpeed * Time.deltaTime);
@@ -102,17 +102,21 @@ public class Movement : MonoBehaviour {
 		anim.SetBool ("w_attack", true);
 		p.y -= 5f;
 		if (Time.time > attackTime) {
+			// Reproducir sonido atacar
 			// atacar(verificar colision con el player y enviarle via metodo que ha sido atacado)
-			player.GetComponent<CharacterScript>().setDamage(10);
+			player.GetComponent<CharacterScript>().setDamage(attackPower);
 			attackTime = Time.time + 1.0f;
 		}
 	}
 	
 	
 	public void setDamage(float damage){
-		health -= damage;
+		health -= damage / defense;
+		// Reproducir sonido golpeado
+		// Reproducir animacion golpeado
 		this.NPCbar.renderer.material.SetFloat("_Cutoff", 1 - (this.health/this.max_health));
 		if (health < 1) {
+			state = "Dead";
 			Debug.Log ("NPC muerto");
 			anim.SetBool("a_walk", false);
 			anim.SetBool("walk", false);
@@ -121,6 +125,38 @@ public class Movement : MonoBehaviour {
 		}
 	}
 
+	private void setHealth(float health){
+		this.health = health;
+		this.max_health = health;
+	}
+
+	private void setDefense(float defense){
+		this.defense = defense;
+	}
+
+	private void setAttackPower(float attackPower){
+		this.attackPower = attackPower;
+	}
+
+	private void setMoveSpeed(float moveSpeed){
+		this.moveSpeed = moveSpeed;
+	}
+
+	private float getHealth(){
+		return this.health;
+	}
+
+	private float getDefense(){
+		return this.defense;
+	}
+
+	private float getMoveSpeed(){
+		return this.moveSpeed;
+	}
+
+	private float getAttackPower(){
+		return this.attackPower;
+	}
 
 	/*void OnTriggerEnter (Collider other){
 		print("Tocado."); 
