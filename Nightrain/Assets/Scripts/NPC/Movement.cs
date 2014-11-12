0e862536ffdcda28a15002653f86d4ba9fc7682b
oriolUB <oriolsuils@gmail.com>
2014-11-12 11:15:13 +0100

    Nuevo script en NPC

--- Nightrain/Assets/Scripts/NPC/Movement.cs
+++ Nightrain/Assets/Scripts/NPC/Movement.cs
@@ -2,57 +2,58 @@
 using System.Collections;
 
 public class Movement : MonoBehaviour {
+	//##############################
+	//Atributos personaje
+	private float moveSpeed = 2; 
 	private float health = 100;
-	private string state;
-	private float damageAtack = 5;
-	
-	private float rotationSpeed = 1.0f;
-	private int moveSpeed = 5;
-
-	private Animator anim;
-	private float attackTime;
-
 	private float max_health = 100;
-	
+	private float defense = 10;
+	private float attackPower = 5;
+	//##############################
+
+	private string state = "None";
+	string[] states = {"Walk", "Find", "Attack", "Dead"};
+	private float rotationSpeed = 1.0f;
+	private float attackTime = 3.0f;
+
 	// Los nombres de los tres puntos que estan distribuidos por el mapa
 	string[] points = {"Point1", "Point2", "Point3"};
 	int j = 0;
 	
-	GameObject player;
-	Transform player_transform;
+	private GameObject player;
+	private Transform player_transform;
+	private Animator anim;
 
 	private GameObject NPCbar;
+	private Music_Engine_Script music;
 	
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag("Player");
 		player_transform = player.transform;
-		anim = GetComponent<Animator>();
-
+		anim = GetComponent<Animator> ();
 		this.NPCbar = GameObject.FindGameObjectWithTag("NPCHealth");
+		this.music = GameObject.FindGameObjectWithTag ("music_engine").GetComponent<Music_Engine_Script> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// Cosas a determinar por el programador de IA
-		float distance_to_player = Vector3.Distance(player_transform.position,transform.position);
-
-		if (distance_to_player < 10) {
-			atack ();
-		} else if (distance_to_player < 60) {
-			perseguir();
-		} else {
-			seguirPuntos();
+		if (!state.Equals("Dead")) {
+			float distance_to_player = Vector3.Distance(player_transform.position,transform.position);
+			if (distance_to_player < 10) {
+					atack ();
+			} else if (distance_to_player < 60) {
+					perseguir ();
+			} else {
+					seguirPuntos ();
+			}
 		}
 	}
 
-	//Return health
-	public float getHealth(){
-		return health;
-	}
-	
 	// Metodo que hace que el personaje vaya uno a uno a los tres puntos del mapa
 	void seguirPuntos(){
+		state = "Walk";
 		anim.SetBool("w_attack", false);
 		anim.SetBool("a_walk", true);
 		anim.SetBool ("walk", true);
@@ -74,6 +75,10 @@ public class Movement : MonoBehaviour {
 	}
 	
 	void perseguir(){
+		if (!state.Equals("Find")) {
+			if(music != null) music.play_Golem_Agresive ();
+		}
+		state = "Find";
 		anim.SetBool("w_attack", false);
 		anim.SetBool("a_walk", true);
 		anim.SetBool ("walk", true);
@@ -82,37 +87,40 @@ public class Movement : MonoBehaviour {
 		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(p - transform.position), rotationSpeed * Time.deltaTime);
 		transform.position += transform.forward * moveSpeed * 2 * Time.deltaTime;
 	}
-	
-	
+
 	// Metodo que rota el NPC unos determinados grados
 	void rotar(int degrees){
 		Quaternion newRotation = Quaternion.AngleAxis (degrees, Vector3.up);
 		transform.rotation = Quaternion.Slerp (transform.rotation, newRotation, rotationSpeed);
 	}
-	
-	
+
 	void atack(){
+		state = "Attack";
 		Vector3 p= player_transform.position;
 		p.y = transform.position.y;
 		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(p - transform.position), rotationSpeed * Time.deltaTime);
-		//Debug.Log("attack");
 		anim.SetBool("a_walk", false);
 		anim.SetBool("walk", false);
 		p.y += 5f;
 		anim.SetBool ("w_attack", true);
 		p.y -= 5f;
 		if (Time.time > attackTime) {
-			// atacar(verificar colision con el player y enviarle via metodo que ha sido atacado)
-			player.GetComponent<CharacterScript>().setDamage(10);
+			player.GetComponent<CharacterScript>().setDamage((int) attackPower);
 			attackTime = Time.time + 1.0f;
+			if(music != null) {
+				music.play_Golem_Agresive();
+				music.play_Golem_Attack();
+			}
 		}
-	}
-	
+	}	
 	
 	public void setDamage(float damage){
-		health -= damage;
+		health -= damage / defense;
+		// Reproducir sonido golpeado
+		// Reproducir animacion golpeado
 		this.NPCbar.renderer.material.SetFloat("_Cutoff", 1 - (this.health/this.max_health));
 		if (health < 1) {
+			state = "Dead";
 			Debug.Log ("NPC muerto");
 			anim.SetBool("a_walk", false);
 			anim.SetBool("walk", false);
@@ -121,6 +129,38 @@ public class Movement : MonoBehaviour {
 		}
 	}
 
+	public void setHealth(float health){
+		this.health = health;
+		this.max_health = health;
+	}
+
+	public void setDefense(float defense){
+		this.defense = defense;
+	}
+
+	public void setAttackPower(float attackPower){
+		this.attackPower = attackPower;
+	}
+
+	public void setMoveSpeed(float moveSpeed){
+		this.moveSpeed = moveSpeed;
+	}
+
+	public float getHealth(){
+		return this.health;
+	}
+
+	public float getDefense(){
+		return this.defense;
+	}
+
+	public float getMoveSpeed(){
+		return this.moveSpeed;
+	}
+
+	public float getAttackPower(){
+		return this.attackPower;
+	}
 
 	/*void OnTriggerEnter (Collider other){
 		print("Tocado."); 
