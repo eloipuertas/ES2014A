156d9cd2893c4723979326a87f55bd1c22f99b52
vcortada123 <vcortada@gmail.com>
2014-12-09 13:58:09 +0100

    Graveler con hijos

---
+++ Nightrain/Assets/Scripts/NPC/Rotar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotar : MonoBehaviour {
+
+	public float velocidadRot = 10.0F;
+	Transform childTransform;
+
+	public void Start(){
+
+		for (int i = 0; i < transform.childCount; i++) {
+
+			childTransform = transform.GetChild(i);
+			print(childTransform.gameObject.name);
+		}
+	}
+
+	public void Update(){
+
+		//childTransform.Rotate (0.0F, velocidadRot*Time.deltaTime, 0.0F);
+
+
+	}
+
+}
---
+++ Nightrain/Assets/Scripts/NPC/rot.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+
+public class rot : MonoBehaviour {
+
+	GameObject go;
+	
+	Transform target;
+
+	// Use this for initialization
+	void Start () {
+		go = GameObject.FindGameObjectWithTag("tag");
+		target = go.transform;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Rotate (0.0F, 1000.0F*Time.deltaTime, 0.0F);
+		transform.RotateAround(transform.parent.position, new Vector3(0, 1, 0), 100.0F * Time.deltaTime);
+	
+
+	}
+}
