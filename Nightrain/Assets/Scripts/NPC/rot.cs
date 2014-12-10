201ae5c1a27c91fc45ccb2316f8092bd625d3fd8
Daniel Urdas <urdas.daniel@yahoo.com>
2014-12-10 11:37:34 +0100

    update sphere script

--- Nightrain/Assets/Scripts/NPC/rot.cs
+++ Nightrain/Assets/Scripts/NPC/rot.cs
@@ -2,41 +2,25 @@
 using System.Collections;
 
 public class rot : MonoBehaviour {
-
+	
 	public GameObject[] spheres;
-
+	
 	public float orbitDistance = 10.0f;
 	public float orbitDegreesPerSec = 180.0f;
-
+	
+	GameObject graveler;
+	Vector3 graveler_position;
+	
 	// Use this for initialization
 	void Start () {
-		
-
+		graveler = this.transform.parent.gameObject;
 	}
-
-	void Orbit(){
-
-		GameObject graveler = this.transform.parent.gameObject;
-		Vector3 graveler_position = graveler.transform.position;
-
-		foreach (Transform trans in this.transform) {
-
-			trans.position = graveler_position + (trans.position - graveler_position).normalized * orbitDistance;
-
-			trans.RotateAround(graveler_position, Vector3.up, orbitDegreesPerSec * Time.deltaTime);
-			
-		}
-
-	}
-
-	void Update () {
-
-	}
-
+	
 	void LateUpdate () {
-		
-		Orbit();
-		
+		graveler_position = graveler.transform.position;
+		foreach (Transform trans in this.transform) {
+			trans.position = graveler_position + (trans.position - graveler_position).normalized * orbitDistance;
+			trans.RotateAround(graveler_position, Vector3.up, orbitDegreesPerSec * Time.deltaTime);	
+		}
 	}
-
 }
